40442e200b2b35ec18c919825c559ae1e54b3d21
manikng <manisskng@gmail.com>
2026-08-28 12:17:17 +0530

    exp1

---
+++ experiments/exp1/js/script.js
@@ -0,0 +1,10 @@
+// Smooth scroll logic for the "Explore CSE Department" link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
